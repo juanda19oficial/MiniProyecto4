168531adc9f3e3aa52fdb59c107d2f5c8a0fe980
juanda19oficial <juan.damian.cuervo@correounivalle.edu.co>
2024-06-15 17:16:17 -0500

    Implemente la funcionalidad para agregar los productos en el archivo de texto y el nodo

--- src/App.java
+++ src/App.java
@@ -4,19 +4,30 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        Inventario inventario = new Inventario();
 
         while (true) {
             try {
+                byte opcion;
                 System.out.println("1. AGREGAR PRODUCTO AL INVENTARIO.");
                 System.out.println("2. ELIMINAR PRODUCTO DEL INVENTARIO.");
-                System.out.println("3. BUSCAR PRODUCTO.");            
+                System.out.println("3. BUSCAR PRODUCTO.");
                 System.out.println("4. ACTUALIZAR CANTIDAD");
                 System.out.println("5. MOSTRAR INVENTARIO");
                 System.out.println("6. SALIR");
-                byte opcion = scanner.nextByte();
+                opcion = scanner.nextByte();
 
                 switch (opcion) {
                     case 1:
+                        // Agregar producto al inventario
+                        System.out.println("Ingrese el código del producto: ");
+                        int codigo = scanner.nextInt();
+                        System.out.println("Ingrese el nombre del producto: ");
+                        scanner.nextLine();
+                        String nombre = scanner.next();
+                        System.out.println("Ingrese la cantidad: ");
+                        int cantidad = scanner.nextInt();
+                        inventario.agregarProducto(codigo, nombre, cantidad);
                         break;
                     case 2:
                         break;
@@ -29,16 +40,17 @@ public class App {
                     case 6:
                         System.exit(0);
                     default:
+                        System.out.println("Opción no válida.");
                         break;
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Error: Debe ingresar un número entre 1 y 6.");
+                scanner.next();
             } catch (NumberFormatException e) {
                 System.out.println("Error: Debe ingresar un número entre 1 y 6.");
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
             }
-            
 
         }
     }
--- src/Inventario.java
+++ src/Inventario.java
@@ -1,8 +1,49 @@
+import filesManagament.manejoArchivos;
+
 public class Inventario {
-    
-    public void agregarProducto(int codigo, String Nombre, int Cantidad){}
-    public void eliminarProducto(int codigo){}
-    public void buscarProducto(int codigo){}
-    public void actualizarCantidad(int codigo, int cantidad){}
-    public void mostrarInventario(){}
+
+    nodoBinario raiz;
+    manejoArchivos archivos;
+
+    public Inventario() {
+        this.raiz = nodoInicial();
+        this.archivos = new manejoArchivos();
+    }
+
+    public nodoBinario nodoInicial() {
+        nodoBinario producto1 = new nodoBinario(500, 150, "ARROZ");
+        producto1.insertar(499, 200, "CARNE", producto1);
+        producto1.insertar(498, 50, "PAPAS", producto1);
+        producto1.insertar(501, 17, "TOMATE", producto1);
+        return producto1;
+    }
+
+    public void agregarProducto(int codigo, String nombre, int cantidad) {
+        nodoBinario nodoInicial = nodoInicial();
+        nodoInicial.insertar(codigo, cantidad, nombre, nodoInicial);
+        archivos.agregarProducto(codigo, nombre, cantidad);
+    }
+
+    public void eliminarProducto(int codigo) {
+        nodoBinario nodoInicial = nodoInicial();
+        nodoInicial = nodoBinario.eliminar(nodoInicial, codigo);
+        archivos.eliminarProducto(codigo);
+
+    }
+
+    public void buscarProducto(int codigo) {
+        nodoBinario nodoInicial = nodoInicial();
+
+    }
+
+    public void actualizarCantidad(int codigo, int cantidad) {
+        nodoBinario nodoInicial = nodoInicial();
+
+    }
+
+    public void mostrarInventario() {
+        nodoBinario nodoInicial = nodoInicial();
+
+    }
+
 }
